63d33ca26726900675de86a344f02f5d0d22faef
Jessica Castaneda <pulsar.ppz70m@gmail.com>
2018-07-09 03:51:24 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -10,7 +10,7 @@ def second_challenge
       :patriarch => {},
       :matriarch => {},
       :heroine => {},
-      :heroine_friends => {}#your key/value pairs here!
+      :heroine_friends => {}
       }}
 
   
